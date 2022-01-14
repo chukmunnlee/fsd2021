ea0ac3850775a8efd84cc43906205dddde439905
chukmunnlee <chukmunnlee@gmail.com>
2022-01-14 08:11:12 +0800

    ADDED: day18

---
+++ day18/src/test/java/ibf2021/ssf/weather/day18/Day18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2021.ssf.weather.day18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
